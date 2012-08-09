819cc34745a3779ec2731fc20ee93d8e24f382d7
Dave Johnson <gearhead@fullsail.edu>
2012-08-08 23:12:19 -0400

    Added first required variables.

--- Johnson_Dave.js
+++ Johnson_Dave.js
@@ -1,1 +1,31 @@
-alert("JavaScript works!");
+/* 
+Dave Johnson
+SDI 1208: Scalable Data & Infrastructures
+Assignment: Project 2
+*/
+
+
+var game = "Skylanders: Spyro's Adventure";
+var characters = 13
+var charNames = new Array ("Spyro", 
+						   "Gill Grunt", 
+						   "Trigger Happy", 
+						   "Ghost Roaster",
+						   "Chop Chop", 
+						   "Voodood",
+						   "Stealth Elf", 
+						   "Flameslinger",
+						   "Prism Break",
+						   "Terrafin",
+						   "Sonic Boom",
+						   "Wrecking Ball",
+						   "Boomer");
+var elements = new Array ("Air",
+						  "Life",
+						  "Earth",
+						  "Fire",
+						  "Undead",
+						  "Water",
+						  "Magic",
+						  "Tech");
+					
