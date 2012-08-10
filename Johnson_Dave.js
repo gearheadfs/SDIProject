93a07ebfe89b7ceb38629d32d282d5e4dcd7a0f9
Dave Johnson <gearhead@fullsail.edu>
2012-08-09 23:43:53 -0400

    Completed Assignment 2.

--- Johnson_Dave.js
+++ Johnson_Dave.js
@@ -5,9 +5,12 @@ Assignment: Project 2
 */
 
 
-var game = "Skylanders: Spyro's Adventure"; //String Global Variable
+var gameTitle = "Skylanders: Spyro's Adventure"; //String Global Variable
+var gameTitleTF = true;
 var charTotal = 32;
 var twoPlayers = true;
+var gavinsChars = true
+var elementals = true
 var charNames = new Array ("Spyro", 
 						   "Gill Grunt", 
 						   "Trigger Happy", 
@@ -59,7 +62,7 @@ var returnPlay = playTogether
 console.log(returnPlay(twoPlayers));
 
 
-//Number function. However I've tried and tried but can't understand why it stops when it hits 22 characters and 10 remaining and then turns the next number, 23, into an actual number.		
+//Number function. However I've tried and tried but can't understand why it stops when it hits 22 characters and 10 remaining and then turns the next number, 23, into an actual number. I assume it has to do with the return.		
 var charRemain = function(charTotal){
 		var currentTotal = 13;
 			while(currentTotal < charTotal){
@@ -75,7 +78,34 @@ console.log(returnTotal(charTotal));
 
 
 //String Function
+var game = function(gameTitle){
+	var awesome = "This game is awesome!";
+	if (gameTitleTF===true){
+		console.log("My son's favorite game is " + gameTitle + " and I'll have to admit it is pretty flippin\' sweet!");
+	} else {
+		console.log("Oh poo, I wish we had " + gameTitle + " because it looks awesome!");
+	};
+	return awesome
+};
+
+var returnString = game;
+console.log(returnString(gameTitle));
 
 
-
+//Array Function
+var listOfChar = function(charNames){
+	var listOfElements = elements
+		if (elementals===true && gavinsChars===true){
+			console.log("Gavin's 13 characters consist of " + charNames + ". Each character belongs to a certain element which there are 8 elements total.");
+		if (gavinsChars===true){
+			console.log("Gavin's favorite is " + charNames[3] + ".");
+		};
+		} else {	
+			console.log("He wants them all!");
+			
+		};
+		return listOfElements
+};
+var returnArray = listOfChar
+console.log(returnArray(charNames));
 
